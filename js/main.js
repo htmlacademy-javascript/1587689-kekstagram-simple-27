68f7233d91799e7b30f674a747301e358052ffa8
Eduard <dumpweed10@gmail.com>
2022-10-09 21:32:14 +0300

    Написал функции создания фотографий

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
-function getNumber(min, max) {
-
-  let randomizeNumber = Math.floor(Math.random() * (max - min + 1) + min)
+function getRandomInteger(min, max) {
 
   if (min < 0 || max < 0) {
     return -1;
@@ -15,17 +13,40 @@ function getNumber(min, max) {
     return -1;
   }
 
-  return randomizeNumber;
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
-
-const MIN = 5;
-const MAX = 20;
-const number = getNumber(MIN, MAX);
-
-console.log(number);
 
 function getComment (string, maxLength) {
   return string <= maxLength;
 }
 
-console.log(getComment (1200, 140));
+console.log(getComment (120, 140));
+
+// module4-task1
+
+const photosDescriptionsArray = [
+  'Описание 1',
+  'Описание 2',
+  'Описание 3',
+  'Описание 4'
+]
+
+const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+
+const photosIdArray = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25'];
+
+const createPhoto = () => ({
+  id:  id = photosIdArray[i],
+  url: 'photos/' + id + '.jpg',
+  description: getRandomArrayElement(photosDescriptionsArray),
+  likes: getRandomInteger(15, 200),
+  comments: getRandomInteger(0, 200)
+})
+
+let photosArray = []
+
+for (i = 0; i <= photosIdArray.length - 1; i++) {
+  photosArray.push(createPhoto());
+}
+
+console.log(photosArray);
