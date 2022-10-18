a3cdfc74d39a4bb1c0226ef1f87a923916669ccf
Keks <keks@htmlacademy.ru>
2022-10-19 01:20:09 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/data.js
@@ -0,0 +1,27 @@
+import {getRandomArrayElement, getRandomInteger} from './utli.js';
+
+const photosDescriptionsArray = [
+  'Описание 1',
+  'Описание 2',
+  'Описание 3',
+  'Описание 4'
+];
+const photosArray = [];
+
+const createPhoto = (index) => ({
+  id:  index,
+  url: `photos/${index}.jpg`,
+  description: getRandomArrayElement(photosDescriptionsArray),
+  likes: getRandomInteger(15, 200),
+  comments: getRandomInteger(0, 200)
+});
+
+const createPhotos = (count) => {
+  for (let i = 1; i <= count; i++) {
+    photosArray.push(createPhoto(i));
+  }
+
+  return photosArray;
+};
+
+export {createPhotos};
