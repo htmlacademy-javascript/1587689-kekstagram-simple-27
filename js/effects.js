70d4d2416d45ded08d904b36be5ead79ab6c6c20
Eduard <dumpweed10@gmail.com>
2022-11-02 01:23:26 +0300

    semicolon

--- js/effects.js
+++ js/effects.js
@@ -4,6 +4,6 @@ const EFFECTS = {
   marvin: { min: 0, max: 100, step: 1, filter: 'invert', units: '%' },
   phobos: { min: 0, max: 3, step: 0.1, filter: 'blur', units: 'px' },
   heat: { min: 1, max: 3, step: 0.1, filter: 'brightness', units: '' }
-}
+};
 
 export { EFFECTS };
