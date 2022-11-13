c91b54fae812f15122d1bccea0214f6576c37ad8
Eduard <dumpweed10@gmail.com>
2022-11-13 16:16:40 +0300

    правки

--- js/api.js
+++ js/api.js
@@ -1,9 +1,9 @@
 const getData = (onSuccess) => {
   fetch('https://27.javascript.pages.academy/kekstagram-simple/data')
-    .then((response) => response.json())
+    .then((response) => response.json());
     .then((photos) => {
       onSuccess(photos);
-    })
+    });
 };
 
 const sendData = (formData, onSuccess, onFail) => {
