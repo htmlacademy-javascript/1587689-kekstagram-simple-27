f3a6440829c2202e406de09fd4547f979b339f90
Eduard <dumpweed10@gmail.com>
2022-11-13 16:19:31 +0300

    линтер

--- js/api.js
+++ js/api.js
@@ -1,6 +1,6 @@
 const getData = (onSuccess) => {
   fetch('https://27.javascript.pages.academy/kekstagram-simple/data')
-    .then((response) => response.json());
+    .then((response) => response.json())
     .then((photos) => {
       onSuccess(photos);
     });
